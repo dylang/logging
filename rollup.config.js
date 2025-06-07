79656a512b90fedc459be38ec9daba31a9975807
Dylan Greene <dgreene@medallia.com>
2025-06-06 20:58:49 -0400

    rollup instead of babel

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+// ESM config
+const esmConfig = {
+  input: {
+    'index': 'src/index.js',
+    'browser': 'src/browser.js',
+    'logging': 'src/logging.js'
+  },
+  output: {
+    dir: 'lib',
+    format: 'esm',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+  },
+  external: ['chalk', 'debug', 'nicely-format'],
+  plugins: [
+    nodeResolve(),
+  ]
+};
+
+// CommonJS config
+const cjsConfig = {
+  input: {
+    'index': 'src/index.js',
+    'logging': 'src/logging.js'
+  },
+  output: {
+    dir: 'lib/cjs',
+    format: 'cjs',
+    entryFileNames: '[name].cjs',
+    preserveModules: true,
+    preserveModulesRoot: 'src',
+  },
+  external: ['chalk', 'debug', 'nicely-format'],
+  plugins: [
+    nodeResolve(),
+    commonjs()
+  ]
+};
+
+export default [esmConfig, cjsConfig];
